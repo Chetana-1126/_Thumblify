b3e047c445570023d7737f4e1a682b64b025d80d
Chetana-1126 <chetanashirode1126@gmail.com>
2026-09-16 11:05:23 +0530

    Initial commit

---
+++ server/routes/UserRoutes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getThumbnailbyId, getUsersThumbnails } from '../controllers/UserController.js';
+
+const UserRouter = express.Router();
+
+UserRouter.get('/thumbnails', getUsersThumbnails)
+UserRouter.get('/thumbnails/:id', getThumbnailbyId)
+
+export default UserRouter;
